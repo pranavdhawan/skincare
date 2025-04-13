859c82d3c136f2ef836769be1deebec6da7d4cbb
Pranav Dhawan <dhawan.pranav8@gmail.com>
2025-04-13 19:28:46 +0530

    env fix

--- config/gemini.ts
+++ config/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 // const genAI = new GoogleGenAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
-const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY; // Store in .env file
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY; // Store in .env file
 const genAI = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 
